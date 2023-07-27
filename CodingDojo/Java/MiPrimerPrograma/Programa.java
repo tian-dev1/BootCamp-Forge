e973c34e43b489107fc7e1d68a1bfc8f6e2eb955
Cristian Camilo <tiancamilo59@gmail.com>
2023-07-27 16:33:17 -0500

    Se trabajo en el ejercicio Mi Primer Programa

---
+++ CodingDojo/Java/MiPrimerPrograma/Programa.java
@@ -0,0 +1,10 @@
+package Java.MiPrimerPrograma;
+
+public class Programa {
+    public static void main(String[] args){
+        System.out.println("Mi nombre es Crsitian");
+        System.out.println("Mi edad es 23");
+        System.out.println("Mi ciudad natal es Bogotá");
+    }
+}
+
